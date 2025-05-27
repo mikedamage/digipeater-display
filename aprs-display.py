d83952cd9e90aac4083c2b8b9c2b96ffea29553e
Mike Green <mike.is.green@gmail.com>
2025-05-26 22:20:11 -0400

    working again using asyncio and aprs3 libs

--- aprs-display.py
+++ aprs-display.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 
+import asyncio
+import attrs
 import time
-import ax25
-import kiss
 import signal
 import sys
 import queue
 import logging
 from pathlib import Path
+from pprint import pformat
+
+# oled display control
 from luma.core.interface.serial import i2c
 from luma.oled.device import ssd1306
 from luma.core.render import canvas
 from PIL import Image, ImageFont
 
+# KISS TNC + APRS packet parsing
+import aprs
+
+
 class Application:
     def __init__(self, host, port):
+        self._host = host
+        self._port = port
         self._logger = self._setupLogger()
         self._display = ssd1306(i2c(port=1, address=0x3c), width=128, height=64, rotate=0)
         self._font_size = 16
         self._font_path = str(Path(__file__).resolve().parent.joinpath('fonts', 'ProggyCleanNerdFontMono-Regular.ttf'))
         self._font = ImageFont.truetype(self._font_path, self._font_size)
         self._connection = None
-        # self._frame_queue = queue.Queue()
         self._received_count = 0
         self._transmitted_count = 0
         self._last_rx_at = None
@@ -30,35 +38,7 @@ class Application:
         self._connected_to_kiss = False
         self._connection_status = 'Connecting'
 
-        # Connect to KISS server to receive packets
         self._init_display()
-        self._logger.info('Connecting to KISS TNC')
-        self._start_receiving(host, port)
-
-    def render(self):
-        current_time = time.strftime('%H:%M:%S')
-        rows = [
-            f'\U0000EB34 KR4BVP-10',
-            f'\U0000F017 {current_time}',
-        ]
-
-        if self._connected_to_kiss:
-            rows.append(f'\U000F00FA: {self._received_count}')
-            # rows.append(f'\U0000F2F5 {self._last_rx_from} {self._last_rx_at}')
-        else:
-            rows.append(f'{self._connection_status}')
-
-        with canvas(self._display) as draw:
-            for i, line in enumerate(rows):
-                draw.text(
-                    (0, 2 + (i * self._font_size)),
-                    text=line,
-                    font=self._font,
-                    fill="white"
-                )
-
-    def cleanup(self):
-        self._stop_receiving()
 
     def _setupLogger(self):
         logger = logging.getLogger('aprs_display')
@@ -73,49 +53,59 @@ class Application:
     def _init_display(self):
         self._display.contrast(1)
 
-    def _start_receiving(self, host, port):
-        def receive_callback(kiss_port, data):
-            self._logger.info('received packet')
-            self._received_count += 1
-            self._last_rx_at = time.strftime('%H:%M:%S')
-            extracted_data = self._extract_frame(data)
-            if not extracted_data:
-                return
-            frame = ax25.Frame.unpack(extracted_data)
-            self._last_rx_from = str(frame.src)
-        try:
-            self._connection = kiss.Connection(receive_callback)
-            self._connection.connect_to_server(host, port)
-            self._connection_status = f'{host}:{port}'
-            self._connected_to_kiss = True
-        except:
-            self._connection_status = 'Not connected'
+    async def _connect(self):
+        self._logger.info('Connecting to KISS TNC')
 
-    def _stop_receiving(self):
-        if self._connection:
-            self._connection.disconnect_from_server()
-            self._connection = None
+        transport, protocol = await aprs.create_tcp_connection(self._host, self._port)
+        self._connection = protocol
+        self._connection_status = "Connected"
+        self._connected_to_kiss = True
 
-    def _extract_frame(self, data):
-        if not (data[0] == 0xC0 and data[1] == 0x00 and data[-1] == 0xC0):
-            return None
-        return data[2:-1].replace(
-            b'\xDB\xDD', b'\xDB').replace(b'\xDB\xDC', b'\xC0')
-    
-    def _process_frames(self):
-        while not self._frame_queue.empty():
-            (frame, timestamp) = self._frame_queue.get()
+    async def render(self):
+        while True:
+            current_time = time.strftime('%H:%M:%S')
+            rows = [
+                f'\U0000EB34 KR4BVP-10',
+                f'\U0000F017 {current_time}',
+            ]
 
-def main():
-    try:
-        app = Application('127.0.0.1', 8001)
+            if self._connected_to_kiss:
+                rows.append(f'\U000F00FA: {self._received_count}')
+                rows.append(f'\U0000F2F5 {self._last_rx_from} {self._last_rx_at}')
+            else:
+                rows.append(f'{self._connection_status}')
+
+            with canvas(self._display) as draw:
+                for i, line in enumerate(rows):
+                    draw.text(
+                        (0, 2 + (i * self._font_size)),
+                        text=line,
+                        font=self._font,
+                        fill="white"
+                    )
+            await asyncio.sleep(1)
+
+    async def receive(self):
+        if not self._connected_to_kiss:
+            await self._connect()
+
+        loop = asyncio.get_running_loop()
+        loop.create_task(self.render())
 
         while True:
-            app.render()
-            time.sleep(1)
-    except KeyboardInterrupt:
-        app.cleanup()
-        sys.exit()
+            async for frame in self._connection.read():
+                self._logger.info("Packet received: %s", pformat(attrs.asdict(frame)))
+                self._received_count += 1
+                self._last_rx_at = time.strftime("%H:%M:%S")
+                self._last_rx_from = frame.source
+
+async def main():
+    app = Application("localhost", 8001)
+    loop = asyncio.get_running_loop()
+    loop.create_task(app.render())
+    loop.create_task(app.receive())
+    loop.run_forever()
 
 if __name__ == "__main__":
-    main()
+    app = Application("localhost", 8001)
+    asyncio.run(app.receive())
